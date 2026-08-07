7b00861f79a21017f4b992f4a696c222aba93c95
eluxone <zdwf2f96nz@privaterelay.appleid.com>
2026-08-07 08:28:05 +0100

    Load BTC features 8 through 12

--- btc-cost-preview.js
+++ btc-cost-preview.js
@@ -1,2 +1,18 @@
 'use strict';
-(()=>{if(document.querySelector('script[data-btc-features]'))return;const s=document.createElement('script');s.src='/btc-features.js';s.defer=true;s.dataset.btcFeatures='1';document.head.appendChild(s)})();
+(()=>{
+  if(document.querySelector('script[data-btc-features]')) return;
+  const loadExtended=()=>{
+    if(document.querySelector('script[data-btc-features-extended]')) return;
+    const x=document.createElement('script');
+    x.src='/btc-features-8-12.js';
+    x.defer=true;
+    x.dataset.btcFeaturesExtended='1';
+    document.head.appendChild(x);
+  };
+  const s=document.createElement('script');
+  s.src='/btc-features.js';
+  s.defer=true;
+  s.dataset.btcFeatures='1';
+  s.addEventListener('load',loadExtended,{once:true});
+  document.head.appendChild(s);
+})();
